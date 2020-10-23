e40330eeb0a333e4ecf6b1fdad69b188a24add3b
Crowdbotics <team@crowdbotics.com>
2020-10-23 21:53:28 +0000

    Update models

--- backend/animation1_21901/wsgi.py
+++ backend/animation1_21901/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animation1_21901.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animation1_21901.settings")
 
 application = get_wsgi_application()
